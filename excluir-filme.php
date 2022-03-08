40fb0f20308cdaffa3e6d48eda66bf1a7500ea78
GabrielObregon <55161709+GabrielObregon@users.noreply.github.com>
2022-03-08 15:11:59 -0300

    Add files via upload
    salvando locadora

---
+++ excluir-filme.php
@@ -0,0 +1,9 @@
+<?php
+include("conexao.php");
+$idFilme = $_GET["idFilme"];
+$sqlDeleta = mysqli_query($conexao,"delete from
+tbFilmes where idFilme = $idFilme")
+or die("Erro ao deletar arquivo. " .
+mysqli_error($conexao));
+header('Location: lista-filmes.php');
+?>
---
+++ gravar-filme.php
@@ -0,0 +1,13 @@
+<?php
+include("conexao.php");
+$tituloFilme = $_POST["tituloFilme"];
+$duracaoFilme = $_POST["duracaoFilme"];
+$valorLocacao = $_POST["valorLocacao"];
+$idCategoria = $_POST["idCategoria"];
+$sqlGravarFilme = mysqli_query($conexao,"insert into
+tbFilmes(tituloFilme,duracaoFilme,valorLocacao,idCat
+egoria)
+value('$tituloFilme','$duracaoFilme','$valorLocacao','$idCategoria')") or die("Erro ao gravar
+registro. " . mysqli_error($conexao));
+header('Location: lista-filmes.php');
+?>
